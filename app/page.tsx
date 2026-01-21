d18711591fe1f9c1d54bfe2f475752fc077ddbf8
ethanjags <ethanjags@berkeley.edu>
2026-01-21 13:33:49 -0800

    fixes:

--- app/page.tsx
+++ app/page.tsx
@@ -12,44 +12,49 @@ export type Ethan = {
   color: string;
   secondaryColor: string;
   thirdColor: string;
+  titleImage: string;
 };
 
 const ethans: Ethan[] = [
   {
     id: 1,
-    name: "Adventurer",
+    name: "adventurer",
     image: "/adventurer.png",
     banner: "/adventurer-banner.png",
     color: "#CBBEFF",
     secondaryColor: "#FFE8BE",
     thirdColor: "#FE4A01",
+    titleImage: "/adventurer-title.png",
   },
   {
     id: 2,
-    name: "Artist",
+    name: "artist",
     image: "/artist.png",
     banner: "/artist-banner.png",
     color: "#EFFFBE",
     secondaryColor: "#800001",
     thirdColor: "#800001",
+    titleImage: "/artist-title.png",
   },
   {
     id: 3,
-    name: "Engineer",
+    name: "engineer",
     image: "/engineer.png",
     banner: "/engineer-banner.png",
     color: "#4300DE",
     secondaryColor: "#FFE8BE",
     thirdColor: "#FFE8BE",
+    titleImage: "/engineer-title.png",
   },
   {
     id: 4,
-    name: "Misc Dude",
+    name: "misc dude",
     image: "/misc-dude.png",
     banner: "/misc-dude-banner.png",
     color: "#FF5100",
     secondaryColor: "#FFE8BE",
     thirdColor: "#FFE8BE",
+    titleImage: "/misc-dude-title.png",
   },
 ];
 
