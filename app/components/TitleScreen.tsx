dcaf8a8b7dcc135e82c14ae0ee0ab9e5cbccff63
ethanjags <ethanjags@berkeley.edu>
2026-02-17 16:34:57 -0800

    fixed sizing for titlescreen

--- app/components/TitleScreen.tsx
+++ app/components/TitleScreen.tsx
@@ -114,8 +114,8 @@ export default function TitleScreen({ ethan, onBack, onEnter }: TitleScreenProps
         >
             <Arrow direction="up" color={ethan.thirdColor} onClick={handleBack} />
             <Image src={'/cloud-long-header.png'} alt={'Clouds'} width={1939} height={592} className="absolute top-0 -translate-y-1/2 left-1/2 -translate-x-1/2 z-0 w-full"/>
-            <div className="absolute w-full left-0 top-[15%] md:top-[60%] md:-translate-y-1/2 z-10">
-                <h1 className="uppercase text-center md:text-left md:mx-20" style={{ color: ethan.secondaryColor, fontSize: 'clamp(3rem, 15vw, 20rem)' }}>The <br/>{ethan.name}</h1>
+            <div className="absolute w-full left-0 top-[15%] md:top-[50%] md:-translate-y-1/2 z-10">
+                <h1 className="uppercase text-center md:text-left md:mx-20" style={{ color: ethan.secondaryColor, fontSize: 'clamp(3rem, 15vw, 15rem)' }}>The <br/>{ethan.name}</h1>
                 <p
                     className="uppercase text-center cursor-pointer hover:opacity-80 transition-opacity"
                     onClick={handleEnter}
